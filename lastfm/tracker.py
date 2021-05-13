fd4ea1ae7a0da065f688ed2314cbb64f7bc71ea0
shihhowie <howardshihhoward@gmail.com>
2021-05-13 14:45:17 +0800

    function to track single user

--- lastfm/tracker.py
+++ lastfm/tracker.py
@@ -9,6 +9,9 @@ from baseline import average_model
 
 curr_path = os.path.dirname(os.path.abspath(__file__))
 
+with open(f"{curr_path}/embeddings/artist_embeddings.json") as f:
+    aid_embedding = json.load(f)
+
 
 def store_embedding(embedding, name, data_id=0):
     if not os.path.isdir(f"{curr_path}/embeddings/{name}"):
@@ -23,7 +26,23 @@ def track_embedding(data_generator, model):
         store_embedding(embedding, model.__str__(), data_id)
 
 
+def track_user(uid, model_name):
+    dir_path = f"{curr_path}/embeddings/{model_name}/"
+    n_windows = len(os.listdir(dir_path))
+    embedding_size = len(list(aid_embedding.values())[0])
+    user_embeddings = np.zeros((n_windows, embedding_size))
+    for i in range(n_windows):
+        fname = dir_path + f"embedding_{i+1}.json"
+        with open(fname) as f:
+            embeddings = json.load(f)
+        null_embedding = np.zeros(embedding_size)
+        uf = embeddings.get(str(uid), null_embedding)
+        user_embeddings[i] = uf
+    return user_embeddings
+
+
 if __name__ == "__main__":
-    data_generator = sliding_window()
-    model = average_model()
-    track_embedding(data_generator, model)
+    # data_generator = sliding_window()
+    # model = average_model()
+    # track_embedding(data_generator, model)
+    print(track_user(1, "baseline"))
